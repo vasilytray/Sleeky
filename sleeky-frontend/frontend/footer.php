14e265fd3b9cd5f1c18218d3ea49933915c59ac7
Vasily Sankov <vsankov@gmail.com>
2022-10-26 15:30:30 +0700

    Update footer.php
    Updated JavaScript Bundle with Popper to bootstrap@5.2.2

--- sleeky-frontend/frontend/footer.php
+++ sleeky-frontend/frontend/footer.php
@@ -1,5 +1,5 @@
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
-
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 <script>
 	// From https://stackoverflow.com/a/30810322
 	function fallbackCopyTextToClipboard(text) {
